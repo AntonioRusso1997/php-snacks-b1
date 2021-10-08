10cb6178f5b19618ee243a2be2c48c0d1e25c499
Antonio <antonioru1997@gmail.com>
2021-10-08 15:06:15 +0200

    SNACK 2 FINITO E FINE ESERCIZIO

--- index.php
+++ index.php
@@ -60,4 +60,36 @@ $matches = [
     for ($i =0; $i < count($matches); $i++) {
         echo '<h2>'.$matches[$i]['homeTeam'] . ' - ' . $matches[$i]['awayTeam'] . ' | ' . '<strong style="color:blue">'.$matches[$i]['homePoints'].'</strong>' . ' - ' . '<strong  style="color:blue">'.$matches[$i]['awayPoints'].'</strong>' .'</h2>';
     };
+
+    echo '<hr><hr>';
+
+
+    //SNACK 2
+
+     //Titolo Snack
+     echo '<h1 style="color:red">SNACK 2:</h1>';
+
+    //Creo variabili get per nome, mail e età
+    $name = $_GET['name'];
+    $mail = $_GET['mail'];
+    $eta = $_GET['eta'];
+
+    //Verificare che il nome sia più lungo di 3 caratteri
+    if (empty($name) && empty($mail) && empty($eta)) {
+        echo '<h2 style="color:red">Error! Inserire tutti i dati</h2>';
+        } else {
+            if (strlen($name) > 3 && strpos($mail, '@') !== false && strpos($mail, '.') !== false && is_numeric($eta) !== false)
+            {
+                echo '<h2 style="color:green">ACCESSO RIUSCITO</h2>';
+
+            } else //Se non rispetta i requisiti
+            {
+                echo '<h2 style="color:red">ACCESSO NEGATO, inserire i dati correttamente.</h2> 
+                <ul>
+                    <li> Il nome deve essere più lungo di 3 caratteri. </li>
+                    <li> la mail deve contenere una @ e un . </li>
+                    <li> Età deve essere un numero </li>
+                </ul> ';
+            }
+        }
 ?>
