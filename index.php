8acd012b807eed33bd6ad48bba96454e291b7314
Antonio <antonioru1997@gmail.com>
2021-10-08 13:19:56 +0200

    Array partite creato

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+// SNACK 1
+
+//Creo array della giornata di campionato
+$matches = [
+    [
+        'homeTeam' => ' Pesaro',
+        'awayTeam' => ' Trieste',
+        'homePoints' => 74,
+        'awayPoints' => 61
+    ],
+    [
+        'homeTeam' => ' Reggiana',
+        'awayTeam' => ' Baket Napoli',
+        'homePoints' => 102,
+        'awayPoints' => 90
+    ],
+    [
+        'homeTeam' => ' Cremona',
+        'awayTeam' => ' Fortitudo Bologna',
+        'homePoints' => 94,
+        'awayPoints' => 78
+    ],
+    [
+        'homeTeam' => ' Virtus Bologna',
+        'awayTeam' => ' Varese',
+        'homePoints' => 97,
+        'awayPoints' => 56
+    ],
+    [
+        'homeTeam' => ' Olimpia Milano',
+        'awayTeam' => ' Trento',
+        'homePoints' => 93,
+        'awayPoints' => 72
+    ],
+    [
+        'homeTeam' => ' Brescia',
+        'awayTeam' => ' Tortona',
+        'homePoints' => 77,
+        'awayPoints' => 79
+    ],
+    [
+        'homeTeam' => ' Brindisi',
+        'awayTeam' => ' Sassari',
+        'homePoints' => 89,
+        'awayPoints' => 80
+    ],
+    [
+        'homeTeam' => ' Treviso',
+        'awayTeam' => ' Venezia',
+        'homePoints' => 80,
+        'awayPoints' => 76
+    ]
+    ];
+    
+
+
+
+
+
+
+?>
