3bcea2919eba9387608a13f5414c68d29121e34b
Antonio <antonioru1997@gmail.com>
2021-10-08 14:17:53 +0200

    SNACK 1 COMPLETATO

--- index.php
+++ index.php
@@ -52,11 +52,12 @@ $matches = [
         'awayPoints' => 76
     ]
     ];
-    
 
+    //Titolo Snack
+    echo '<h1 style="color:red">SNACK 1:</h1>';
 
-
-
-
-
+    //Ciclo per stampare tutte le partite
+    for ($i =0; $i < count($matches); $i++) {
+        echo '<h2>'.$matches[$i]['homeTeam'] . ' - ' . $matches[$i]['awayTeam'] . ' | ' . '<strong style="color:blue">'.$matches[$i]['homePoints'].'</strong>' . ' - ' . '<strong  style="color:blue">'.$matches[$i]['awayPoints'].'</strong>' .'</h2>';
+    };
 ?>
